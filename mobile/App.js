7533511a773e6bba3ba5de282e0f98aea92a35aa
Yan <yankaiquecosta.yk@gmail.com>
2020-07-25 20:11:14 -0300

    Redux and redux-persist configured

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import {store, persistor} from './src/Store';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default () =>{
+  return(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
