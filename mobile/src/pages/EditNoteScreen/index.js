5bbfa8783cd2b0ae09361f1e9f3bd4b320a4e733
Yan <yankaiquecosta.yk@gmail.com>
2020-07-27 20:50:29 -0300

    Structure and style

--- mobile/src/pages/EditNoteScreen/index.js
+++ mobile/src/pages/EditNoteScreen/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
     Container,
-    Text
+    TitleInput,
+    BodyInput
 } from './styles';
 
 
 export default () =>{
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+
     return(
         <Container>
-            <Text>Edit notes Screen</Text>
+            <TitleInput 
+                value={title}
+                onChangeText={t=>setTitle(t)}
+                placeholder="Digite o título da anotação" 
+                placeholderTextColor="#CCC"
+                autoFocus={true}    
+            />
+            <BodyInput 
+                value={body}
+                onChangeText={t=>setBody(t)}
+                placeholder="Digite o corpo da anotação" 
+                placeholderTextColor="#CCC" 
+                multiline={true}
+                textAlignVertical="top"
+            />
         </Container>
     )
 }
--- mobile/src/pages/EditNoteScreen/styles.js
+++ mobile/src/pages/EditNoteScreen/styles.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-
+    flex:1;
+    background-color:#333;
 
 `;
 
-export const Text = styled.Text`
+export const TitleInput = styled.TextInput`
+    font-size:20px;
+    font-weight: bold;
+    padding: 12px;
+    color:#fff;
+    border-style:solid;
+    border-bottom-color: #000;
+    border-bottom-width:1px;
+`;
 
+export const BodyInput = styled.TextInput`
+    flex:1;
+    padding:15px;
+    font-size:15px;
+    color:#FFF;
 `;
